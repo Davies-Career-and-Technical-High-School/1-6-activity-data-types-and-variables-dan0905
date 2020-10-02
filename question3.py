929c6dcdc2f531e46344ff48d1b6f2d72ad5e28c
Daniel Quiroa <daniel.quiroa@daviestech.org>
2020-10-02 15:26:54 +0000

    #3

--- question3.py
+++ question3.py
@@ -1,11 +1,11 @@
 animal = input("Enter an animal: ")
 sound = input ("Enter a sound: ")
 
-e = "E"
+e = "E-I-E-I-O"
 
 print ("Old Macdonald had a farm, " + e)
-print ("And on his farm he had a" + animal + "," + e)
-print ("With a " + animal + "-" + animal + " here and a" + sound + "-" + sound + " there")
+print ("And on his farm he had a " + animal + ", " + e)
+print ("With a " + sound + " - " + sound + " here and a " + sound + " - " + sound + " there")
 print ("Here a "+ sound+ " there a " +sound)
-print ("Everywhere a" + sound + "-" + animal )
+print ("Everywhere a " + sound + " - " + sound )
 print ("Old Macdonald had a farm," + e)
